8ba086ac5fc2a605499f41ff1a113a957cf402ba
poornimag97 <126610347+poornimag97@users.noreply.github.com>
2023-03-11 14:49:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Parent from './Parent';
+
+const App=()=>{
+  return(
+    <>
+   < Parent data={100}/>
+    </>
+  )
+}
+export default App;
---
+++ Child.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const Child=(props)=>{
+    return (
+        <>
+         <h1>{props.data}</h1>
+        </>
+    )
+}
+export default Child;
